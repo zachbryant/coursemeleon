2bc3460d54a26fdd767e9b748d4716ef6675cf1e
lauren <lauren.lum50@gmail.com>
2019-02-06 21:55:32 -0500

    added help page

--- src/router.js
+++ src/router.js
@@ -21,6 +21,15 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/help",
+      name: "help",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/Help.vue")
     }
   ]
 });
