17657331e0dc0e8f3aa02f6bbcb1476863882326
zachbryant <zbryant832@gmail.com>
2019-02-10 16:17:42 -0500

    converted html to pug, and created navbar

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,46 @@
               img#navlogo(src='./assets/logo_lightgreen_300.svg' alt='Coursemeleon logo')
               span#navtitle Coursemeleon
         v-flex#searchbar(xs4)
-          searchbar
+          v-autocomplete(
+            v-model="model"
+            label="Search Purdue courses"
+            :loading="isLoading ? 'loading' : undefined"
+            :items="items"
+            search-input.sync="search"
+            cache-items
+            hide-no-data
+            clearable
+            append-icon=""
+            @change="queryCourses"
+          )
         v-flex#navitems(xs4)
           router-link(to='/explore') Explore
           router-link(to='/about') About
           router-link(to='/help') Help
       router-view
 </template>
+
+<script>
+  export default{
+    name: "app",
+    data () {
+      return {
+        model: null,
+        isLoading: false,
+        items: ["CS 307", "MA 128", "ECE 376", "PHYS 172"],
+      }
+    },
+    methods: {
+      queryCourses(queryString) {
+        if (queryString != undefined && queryString.length > 1) {
+          this.isLoading = true;
+          console.log(queryString);
+          this.isLoading = false;
+        }
+      }
+    }
+  }
+</script>
 
 <style lang="less">
   @import (css) url('https://fonts.googleapis.com/css?family=Nunito');
@@ -38,16 +71,18 @@
   }
 
   #navitems a {
-    margin-left: 2em;
+    padding-left: 1vw;
+    padding-right: 1vw;
   }
   
   #nav {
-    padding: 30px;
+    padding-top: 1.9vh;
+    padding-bottom: 1.9vh;
     a {
       font-weight: @semibold;
       text-decoration: none;
       color: @primary;
-      font-size: 1.5em;
+      font-size: calc(0.5vw + 12px);
       &.router-link-exact-active {
         //color: #42b983;
         font-weight: @semibold;
@@ -66,7 +101,7 @@
   }
 
   #navtitle {
-    font-size: 2em;
+    font-size: 2.5vw;
     font-weight: bold;
   }
 </style>
