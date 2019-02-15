277b1597c21b82af23048d33393c8f5c1538b9cb
Zach Bryant <zbryant832@gmail.com>
2019-02-14 20:47:47 -0500

    merge home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,14 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
 
-<style lang="less">
-
-</style>
+<style lang="less"></style>
