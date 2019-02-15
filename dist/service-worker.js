976e24762f47cd28cebfd14a8c96167228e332ea
llum602 <32022929+llum602@users.noreply.github.com>
2019-02-15 13:49:43 -0500

    merged app.vue with master on my branch

---
+++ dist/service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.3f047d2962a0f813d5d949fe581c409b.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "coursemeleon"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
-<template>
-  <div id="app">
-    <Navigation />
-    
-    <router-view />
-  </div>
+<template lang="pug">
+  v-app#app(light)
+    Navigation
+    v-content
+      v-container(fluid align-content-center fill-height fill-width style="padding-top: 0;")
+        v-layout(column justify-start)
+          v-layout#nav(row justify-center)
+            v-flex(xs12)
+              //- @TODO navbar
+          v-flex(xs12)
+            router-view
 </template>
 
 <script>
@@ -11,6 +16,19 @@ import Navigation from "@/components/Navigation.vue";
 
 export default {
   name: "app",
+  data() {
+    return {};
+  },
+  methods: {
+    queryCourses(queryString) {
+      if (queryString != undefined && queryString.length > 1) {
+        this.isLoading = true;
+        console.log(queryString);
+        //@TODO insert api call
+        this.isLoading = false;
+      }
+    }
+  },
   components: {
     Navigation
   }
@@ -18,13 +36,23 @@ export default {
 </script>
 
 <style lang="less">
+@import (css) url("https://fonts.googleapis.com/css?family=Nunito");
+@import (css)
+  url("https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+@primary: var(--v-primary-base);
+@secondary: var(--v-secondary-base);
+@accent: var(--v-accent-base);
+@semibold: 600;
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Nunito", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  //color: @primary;
+  height: 100vh;
 }
+
 #nav {
   padding: 30px;
   a {
