7d3ad58a9950341e4f16bfb9b07428846698788b
Zach Bryant <zbryant832@gmail.com>
2019-02-17 17:08:50 -0500

    init course page

---
+++ src/components/CoursePage.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+
+</template>
+
+<script>
+
+export default {
+	name: "coursepage",
+	components: {
+
+	},
+	props: {
+
+	},
+	data() {
+		return {
+			
+		}
+	},
+	methods: {
+
+	},
+}
+</script>
+
+<style>
+
+</style>
