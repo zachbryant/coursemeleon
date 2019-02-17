537f7a459c9364d72d15f9376b217232d1fc81b4
Zach Bryant <zbryant832@gmail.com>
2019-02-17 17:09:18 -0500

    init sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+
+</template>
+
+<script>
+
+export default {
+	name: "sidebar",
+	components: {
+
+	},
+	props: {
+		
+	},
+	data() {
+		return {
+			
+		}
+	},
+	methods: {
+
+	},
+}
+</script>
+
+<style>
+
+</style>
