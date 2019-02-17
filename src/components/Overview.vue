1cfc2eac33599340e737b255c53ceb0e466b8987
Zach Bryant <zbryant832@gmail.com>
2019-02-17 17:09:04 -0500

    init overview component

---
+++ src/components/Overview.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+
+</template>
+
+<script>
+
+export default {
+	name: "overview",
+	components: {
+
+	},
+	props: {
+
+	},
+	data() {
+		return {
+
+		}
+	},
+	methods: {
+
+	},
+}
+</script>
+
+<style>
+
+</style>
