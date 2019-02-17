e1123b4140baf8c975d1a7571e177fe6a20aad8f
Zach Bryant <zbryant832@gmail.com>
2019-02-17 17:08:19 -0500

    Added optional course page prop to home

--- src/router.js
+++ src/router.js
@@ -5,34 +5,35 @@ import Home from "./views/Home.vue";
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      // Not lazy loading b/c we want this to be fast
-      component: Home
-    },
-    {
-      path: "/explore",
-      name: "explore",
-      // route level code-splitting (lazy load
-      component: () =>
-        import(/* webpackChunkName: "explore" */ "./views/Explore.vue")
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting (lazy load)
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    },
-    {
-      path: "/help",
-      name: "help",
-      // route level code-splitting (lazy load
-      component: () => import(/* webpackChunkName: "help" */ "./views/Help.vue")
-    }
-  ]
+	mode: "history",
+	base: process.env.BASE_URL,
+	routes: [
+		{
+			path: "/:selectedCoursePage",
+			name: "home",
+			// Not lazy loading b/c we want this to be fast
+			component: Home,
+		},
+		{
+			path: "/explore",
+			name: "explore",
+			// route level code-splitting (lazy load
+			component: () =>
+				import(/* webpackChunkName: "explore" */ "./views/Explore.vue"),
+		},
+		{
+			path: "/about",
+			name: "about",
+			// route level code-splitting (lazy load)
+			component: () =>
+				import(/* webpackChunkName: "about" */ "./views/About.vue"),
+		},
+		{
+			path: "/help",
+			name: "help",
+			// route level code-splitting (lazy load
+			component: () =>
+				import(/* webpackChunkName: "help" */ "./views/Help.vue"),
+		},
+	],
 });
