9949ba454e73ea0bcd56279c66f5fde2a8b6f299
Zach Bryant <zbryant832@gmail.com>
2019-02-19 03:09:24 -0500

    add path to home without props

--- src/router.js
+++ src/router.js
@@ -5,36 +5,40 @@ import Home from "./views/Home.vue";
 Vue.use(Router);
 
 export default new Router({
-	mode: "history",
-	base: process.env.BASE_URL,
-	routes: [
-		{
-			path: "/:selectedCoursePage",
-			name: "home",
-			// Not lazy loading b/c we want this to be fast
-			component: Home,
-			props: true,
-		},
-		{
-			path: "/explore",
-			name: "explore",
-			// route level code-splitting (lazy load
-			component: () =>
-				import(/* webpackChunkName: "explore" */ "./views/Explore.vue"),
-		},
-		{
-			path: "/about",
-			name: "about",
-			// route level code-splitting (lazy load)
-			component: () =>
-				import(/* webpackChunkName: "about" */ "./views/About.vue"),
-		},
-		{
-			path: "/help",
-			name: "help",
-			// route level code-splitting (lazy load
-			component: () =>
-				import(/* webpackChunkName: "help" */ "./views/Help.vue"),
-		},
-	],
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/:selectedCoursePage",
+      name: "home",
+      component: Home,
+      props: true
+    },
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+      props: false
+    },
+    {
+      path: "/explore",
+      name: "explore",
+      // route level code-splitting (lazy load
+      component: () =>
+        import(/* webpackChunkName: "explore" */ "./views/Explore.vue")
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting (lazy load)
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/help",
+      name: "help",
+      // route level code-splitting (lazy load
+      component: () => import(/* webpackChunkName: "help" */ "./views/Help.vue")
+    }
+  ]
 });
