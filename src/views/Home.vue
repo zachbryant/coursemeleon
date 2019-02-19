12fb03b42480f1a856bcf0af0bee3669b46c351a
Zach Bryant <zbryant832@gmail.com>
2019-02-19 03:11:52 -0500

    Added Sidebar, Course page, and overview components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,48 @@
 <template lang="pug">
-  v-container(fluid fill-height)
+  v-container(fluid fill-height py-5)
     v-layout(justify-space-between row fill-height)
-      v-flex(xs4)
-        v-card(color="light-green lighten-3") hi
+      v-flex#flexSidebar(xs4)
+        Sidebar
       v-spacer
       v-flex(xs8)
-        v-card(color="light-green lighten-3") hi
+        CoursePage(v-if="checkSelectedCoursePage()" course="selectedCoursePage")
+        Overview(v-else)
 </template>
 
 <script>
 // @ is an alias to /src
+import Sidebar from "@/components/Sidebar";
+import Overview from "@/components/Overview";
+import CoursePage from "@/components/CoursePage";
 
 export default {
   name: "home",
-  components: {}
+  components: {
+    Sidebar,
+    Overview,
+    CoursePage
+	},
+	props: {
+    selectedCoursePage: {
+      type: String,
+      required: false
+    }
+  },
+	data() {
+		return {
+      coursePage: this.selectedCoursePage,
+		}
+	},
+	methods: {
+    checkSelectedCoursePage() {
+      return this.coursePage != null && this.coursePage.length > 4;
+    }
+	},
 };
 </script>
 
 <style lang="less">
-.v-card {
-  height: 100%;
-  margin-left: 0.4vw;
-  margin-right: 0.4vw;
+#flexSidebar {
+  max-width: 500px;
 }
 </style>
