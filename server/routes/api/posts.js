925de7c429ed497d46535cebd6f23be4a52b7aa2
llum602 <32022929+llum602@users.noreply.github.com>
2019-03-10 17:00:17 -0500

    added template backend

---
+++ server/routes/api/posts.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+//Get Posts
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({/*query*/}).toArray());
+});
+
+//Add Post
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+});
+
+//Delete Post
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+});
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect('mongodb+srv://user:7987@coursemeleon-slknn.mongodb.net/test?retryWrites=true', {
+        useNewUrlParser: true
+    })
+
+    return client.db('coursemeleon').collection('posts');
+}
+
+module.exports = router;
--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -2,17 +2,20 @@
   <div>
     <v-container grid-list-xl></v-container>
     <h1>Explore</h1>
-    <Calendar/>
+    
+    <PostComponent/>
   </div>
 </template>
 
 <script>
 import Calendar from "@/components/Calendar.vue";
+import PostComponent from "@/components/PostComponent.vue";
 
 export default {
  name: "explore",
  components: {
-   Calendar
+   Calendar,
+   PostComponent
  }
 };
 </script>
