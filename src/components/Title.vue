f92483545477ce213d30284e8c5a579eb20c997c
llum602 <32022929+llum602@users.noreply.github.com>
2019-03-11 21:28:06 -0500

    added title component

---
+++ src/components/Title.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <v-container grid-list-xl></v-container>
+        <!-- below just gets the first item-->
+        <h1>{{ posts[0].title }}</h1>
+        <v-container grid-list-xl></v-container>
+    </div>
+</template>
+
+<script>
+import PostService from '../PostService';
+
+export default {
+  name: "Title",
+  data() {
+      return {
+          posts: [],
+          error: '',
+          text: ''
+      }
+  },
+  async created() { //runs automatically when component created
+    try {
+        this.posts = await PostService.getPosts();
+    } catch(err) {
+        this.error = err.message;
+    }
+  },
+  methods: {
+      async createPost() {
+          await PostService.insertPost(this.text);
+          this.posts = await PostService.getPosts();
+      },
+      async deletePost(id) {
+          await PostService.deletePost(id);
+          this.posts = await PostService.getPosts();
+      }
+  }
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
+const path = require('path');
+
 module.exports = {
   lintOnSave: process.env.NODE_ENV !== "production",
+  outputDir: path.resolve(__dirname, '/server/public'),  //set output path
   devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000'
+      }
+    },
     open: true,
     host: "localhost",
     port: 8080,
