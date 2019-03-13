ec1dd0cfda1212274a615bd6328ccbd60026c3a2
Zach Bryant <zbryant832@gmail.com>
2019-03-13 02:55:25 -0400

    scrolling capabilities & max-height, css variables

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,9 @@
 <template lang="pug">
-  v-container(fluid fill-height py-5)
-    v-layout(justify-space-between row fill-height)
-      v-flex#flexSidebar(xs4)
+  v-container#home(fluid fill-height py-5)
+    v-layout(row)
+      v-flex#flexSidebar(xs4 scroll-y)
         Sidebar
-      v-spacer
-      v-flex(xs8)
+      v-flex#coursePage(xs10 justify-center scroll-y)
         keep-alive
           component(:is="currentCourseComponent" :data="currentCourseName")
 </template>
@@ -51,8 +50,29 @@ export default {
 </script>
 
 <style lang="less">
+//Variables
+@primary: var(--v-primary-base);
+@secondary: var(--v-secondary-base);
+@accent: var(--v-accent-base);
+@text-color: #333333;
+@semibold: 600;
+@regular: 400;
+
+#home {
+  max-height: 99vh;
+}
+
 #flexSidebar {
   max-width: 25vw;
   min-width: 15vw;
+  border-right: 1px solid @primary;
+}
+#coursePage {
+  padding: 0 5% 0 5%;
+}
+
+#coursePage,
+#flexSidebar {
+  min-height: 100%;
 }
 </style>
