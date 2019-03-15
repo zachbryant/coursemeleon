bcb030bf463f02b27575bdaebc17d7bb07db633f
Zach Bryant <zbryant832@gmail.com>
2019-03-15 03:51:12 -0400

    authentication & route for course editing

--- src/router.js
+++ src/router.js
@@ -1,17 +1,35 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import store from "./store";
 
 Vue.use(Router);
 
-export default new Router({
+function isObjectEmpty(obj) {
+  return Object.keys(obj).length === 0 && obj.constructor === Object;
+}
+
+let router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
       path: "/",
-      name: "overview",
-      component: Home
+      name: "home",
+      component: Home,
+      meta: {
+        requiresAuthQueries: true
+      }
+    },
+    {
+      path: "/manage:cid",
+      name: "manage",
+      component: () => import("./views/Manage.vue"),
+      alias: "/edit",
+      meta: {
+        requiresAuth: true
+      },
+      props: true
     },
     {
       path: "/explore",
@@ -21,7 +39,8 @@ export default new Router({
     {
       path: "/login",
       name: "login",
-      component: () => import("./views/Login.vue")
+      component: () => import("./views/Login.vue"),
+      props: true
     },
     {
       path: "/about",
@@ -41,3 +60,27 @@ export default new Router({
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => !!record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next();
+      return;
+    }
+    next({ path: "/login", props: to.props });
+    return;
+  }
+  if (to.matched.some(record => !!record.meta.requiresAuthQueries)) {
+    if (store.getters.isLoggedIn) {
+      next();
+      return;
+    }
+    if (!isObjectEmpty(to.query)) {
+      next({ path: "/login", props: to.props, query: to.query });
+      return;
+    }
+  }
+  next();
+});
+
+export default router;
