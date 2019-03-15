0601591fdc48dc882ad7ae11455abf7742f3b5b0
Zach Bryant <zbryant832@gmail.com>
2019-03-15 00:10:28 -0400

    finished login UI

---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  login
+</template>
+
+<script>
+import Login from "@/components/Login.vue";
+
+export default {
+  name: "loginPage",
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {
+    Login
+  },
+  computed: {}
+};
+</script>
+
+<style lang="less">
+@import (reference) "../App.less";
+</style>
