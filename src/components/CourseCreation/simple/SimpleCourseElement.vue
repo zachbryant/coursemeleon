8e77377eb56381083c468cd615eb0f31c6632434
zachbryant <zbryant832@gmail.com>
2019-03-17 18:38:35 -0400

    base course element

---
+++ src/components/CourseCreation/simple/SimpleCourseElement.vue
@@ -0,0 +1,37 @@
+<template lang="pug"></template>
+
+<script>
+export default {
+    name: "simpleCourseElement",
+    props: {
+        data: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        getType() {
+            return name;
+        },
+        getComponents() {
+            console.log("No sub components for simpleCourseElement");
+            return [];
+        },
+        getData() {
+            var allData = {
+                type: getType(),
+                data: () => {
+                    var dataList = [];
+                    for (component in getComponents()) {
+                        dataList.push(component.getData());
+                    }
+                    return dataList;
+                }
+            };
+            return allData;
+        }
+    }
+}
+</script>
+
+<style lang="less"></style>
