85af17d61e7f3b1b6178485206d847a48cb629f5
zachbryant <zbryant832@gmail.com>
2019-03-17 18:38:56 -0400

    placeholder simple components

---
+++ src/components/CourseCreation/simple/Text.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+    
+</template>
+
+<script>
+import SimpleCourseElement from "SimpleCourseElement.vue";
+
+export default {
+    name: "text",
+    extends: SimpleCourseElement,
+    data() {
+        
+    }
+}
+</script>
+
+<style lang="less">
+
+</style>
