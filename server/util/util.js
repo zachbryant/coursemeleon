ff16960597e3a5ab1bf7ffab084d9952aebb814a
Zach Bryant <zbryant832@gmail.com>
2019-03-31 02:41:24 -0400

    misc util funcs

--- server/util/util.js
+++ server/util/util.js
@@ -1,46 +1,6 @@
-const mongodb = require("mongodb");
+const mongoose = require("mongoose");
 
-module.exports = {
-  getdb: async function() {
-    const client = await mongodb.MongoClient.connect(
-      "mongodb+srv://user:7987@coursemeleon-slknn.mongodb.net/test?retryWrites=true",
-      { useNewUrlParser: true }
-    );
-    return await client.db("coursemeleon");
-  },
-  _db: this.getdb,
-  isValidEmail: function(email) {
-    const emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    return emailPattern.test(String(email).toLowerCase());
-  },
-
-  isAuthorized: function(token) {
-    console.log("TODO isAuthorized");
-    return !!token;
-  },
-
-  queryOne: function(collection, query, projection) {
-    return this._db
-      ? this._db.collection(collection).findOne(query, projection)
-      : { error: "MongoDB client/db invalid in util.js:1" };
-  },
-
-  query: function(collection, query, projection) {
-    return this._db
-      ? this._db.collection(collection).find(query, projection)
-      : { error: "MongoDB client/db invalid in util.js:1" };
-  },
-
-  removeOne: function(collection, projection, callback) {
-    return this._db
-      ? this._db.collection(collection).removeOne(projection, callback)
-      : { error: "MongoDB client/db invalid in util.js:1" };
-  },
-
-  updateOne: function(collection, filter, update) {
-    console.log(this._db);
-    return this._db
-      ? this._db.collection(collection).updateOne(filter, update)
-      : { error: "MongoDB client/db invalid in util.js:1" };
-  }
+module.exports.isValidEmail = function(email) {
+  const emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  return emailPattern.test(String(email).toLowerCase());
 };
