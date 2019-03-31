65f2fc3b0dc563fb5874b8f53e05c6cf3a4ea723
Zach Bryant <zbryant832@gmail.com>
2019-03-31 02:37:37 -0400

    organization & config

--- server/index.js
+++ server/index.js
@@ -1,33 +1,29 @@
-const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
-var multer = require("multer");
-var cookieSession = require("cookie-session");
+const multer = require("multer");
+const express = require("express");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 
 const app = express();
 var upload = multer();
 
-//Middleware
+// Configuration
+require("./config/passport");
+require("./config/mongo");
+require("./config/jwtConfig");
+
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.array());
-app.use(
-  cookieSession({
-    name: "coursemeleon",
-    keys: ["authrandomkey"],
-    maxAge: 24 * 60 * 60 * 1000 // 24 hours
-  })
-);
 app.use(cors());
 app.use(passport.initialize()); // Start using passport
 app.use(passport.session()); // Session management
 
+// Routes
 const auth = require("./routes/api/auth");
 const posts = require("./routes/api/posts");
 const course = require("./routes/api/course");
-
-// Routes
 app.use("/api/posts", posts);
 app.use("/api/course", course);
 app.use("/api/auth", auth);
