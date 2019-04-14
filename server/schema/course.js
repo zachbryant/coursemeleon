121dcc31c201eef51b77506e920e495e2aea3c47
Zach Bryant <zbryant832@gmail.com>
2019-04-13 20:40:06 -0400

    cleaned up syntax

--- server/schema/course.js
+++ server/schema/course.js
@@ -10,22 +10,44 @@ var courseSchema = new Schema({
     default: uuidv4()
   },
   term: String,
-  term_start: Date
+  term_start: Date,
+  announcements: {
+    type: String,
+    required: false,
+    default: ""
+  }
 });
 // Index on course name ASCENDING and within each group term_start DESCENDING
 courseSchema.index({ cname: 1, term_start: -1 });
 
-courseSchema.statics.byName = function(name, callback) {
-  return this.find({ name: new RegExp(name, "i") }, callback);
+// For doing general work on "Courses"
+courseSchema.statics = {
+  byCid: function(cid, callback) {
+    return this.findOne({ cid: cid }, callback);
+  },
+  byName: function(name, callback) {
+    return this.find({ cname: new RegExp(name, "i") }, callback);
+  },
+  byAbbr: function(abbr, callback) {
+    return this.find({ abbr: new RegExp(abbr, "i") }, callback);
+  },
+  exact: function(params, callback) {
+    return this.findOne(params, callback);
+  },
+  byTermStart: function(termStart, callback) {
+    return this.find({ term_start: termStart }, callback);
+  }
 };
-courseSchema.statics.byAbbr = function(abbr, callback) {
-  return this.find({ name: new RegExp(abbr, "i") }, callback);
-};
-courseSchema.statics.byTermStart = function(termStart, callback) {
-  return this.find({ term_start: termStart }, callback);
-};
-courseSchema.statics.exact = function(params, callback) {
-  return this.findOne(params, callback);
+
+// For doing instance work on a Course
+courseSchema.methods = {
+  updateObject: function(obj, callback) {
+    let self = this;
+    Object.keys(obj).map(key => {
+      if (key in self) self[key] = obj[key];
+    });
+    this.save(callback);
+  }
 };
 
 var Course = mongoose.model("Course", courseSchema, "Courses");
