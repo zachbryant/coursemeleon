a352b052180b4a6b37f6d712b366333155304e08
Zach Bryant <zbryant832@gmail.com>
2019-04-15 08:28:47 -0400

    on create initialize a dummy course object

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,21 @@
   v-container#home(fluid fill-height px-5)
     v-layout(row)
       keep-alive
-        component(v-if="isCreate" :is="currentCourseComponent" :course="courseData")
+        component(v-if="isCreate" :is="currentCourseComponent")
         component(v-if="validQuery" :is="currentCourseComponent" :search="query")
         
 </template>
 
 <script>
 // * @ is an alias to /src
-import { Overview, Course } from "@/components/componentImports";
+import { Overview } from "@/components/componentImports";
+const uuidv4 = require("uuid/v4");
 
 export default {
   name: "home",
   components: {
     Overview,
-    Course
+    course: () => import("@/components/Course/Course")
   },
   props: {
     query: {
@@ -31,23 +32,12 @@ export default {
     }
   },
   data() {
-    return {
-      courseData: {
-        title: "",
-        elements: [],
-        term: "",
-        term_start: "",
-        color: "",
-        font: "",
-        published: false,
-        whitelist: false
-      }
-    };
+    return {};
   },
   methods: {
     validQuery() {
       return (
-        !!this.query.name ||
+        !!this.query.title ||
         !!this.query.cid ||
         !!this.query.term ||
         !!this.query.abbr
@@ -56,7 +46,39 @@ export default {
   },
   computed: {
     currentCourseComponent: function() {
-      return this.isCreate || this.validQuery() ? Course : "overview";
+      return this.isCreate || this.validQuery() ? "course" : "overview";
+    }
+  },
+  created() {
+    if (this.isCreate) {
+      this.$store.commit("toggleEditMode");
+      this.$store.commit("setActiveCourse", {
+        title: "",
+        cid: uuidv4(),
+        tabs: [
+          {
+            title: "",
+            id: uuidv4(),
+            elements: [
+              {
+                instanceName: "text-item",
+                id: uuidv4(),
+                data: {
+                  type: "h1",
+                  text: ""
+                }
+              }
+            ]
+          }
+        ],
+        abbr: "",
+        term: "",
+        term_start: "",
+        color: "",
+        font: "",
+        published: false,
+        whitelist: false
+      });
     }
   }
 };
