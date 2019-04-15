9854715ce6d1d17e01ed86ff02f7095267fc7f2e
Zach Bryant <zbryant832@gmail.com>
2019-04-15 08:23:33 -0400

    uuid util function

--- server/util/util.js
+++ server/util/util.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const uuidv4 = require("uuid/v4");
 
-
-module.exports.isValidEmail = function(email) {
-  const emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-  return emailPattern.test(String(email).toLowerCase());
-};
-
-module.exports.getUUIDV4 = function() {
-  return uuidv4();
+module.exports = {
+  isValidEmail: function(email) {
+    const emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return emailPattern.test(String(email).toLowerCase());
+  },
+  getUUIDV4: function() {
+    return uuidv4();
+  },
+  isValidJWT(jwt_payload) {
+    return !jwt_payload.expires || jwt_payload.expires > Date.now();
+  }
 };
