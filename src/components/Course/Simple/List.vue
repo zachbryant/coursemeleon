a25a719469027ae0e41a301dbcb6ce27653b7d51
Zach Bryant <zbryant832@gmail.com>
2019-04-15 08:32:23 -0400

    computed props cover for store, assign indices to rendered objects

--- src/components/Course/Simple/List.vue
+++ src/components/Course/Simple/List.vue
@@ -1,16 +1,17 @@
 <template lang="pug">
   v-layout(column align-start justify-center fill-width)
     template(v-if="isEditMode")
-      v-layout(row fill-width justify-start)
+      //-v-layout(row fill-width justify-start)
         v-flex(xs1)
         v-flex(xs11)
           edit-sep(:index="-1"
                 v-on:edit-sep-new="editSepNew"
                 :show="canShowInsert(-1)")
-      draggable(:list="data.elements" class="fill-width")
+      draggable(:list="elements" class="fill-width" type="transition" name="flip-list" draggable=".item")
         transition-group(class="fill-width")
           v-layout(row v-for="(comp, index) in elements" 
-                  :key="comp.id" 
+                  :key="index" 
+                  :class="index > 0 ? 'item' : ''"
                   fill-width 
                   justify-start
                   @mouseover="hoverIndex(index)"
@@ -22,13 +23,16 @@
                             v-show="canShowMenu(index)")
             v-flex(xs11 grow)
               v-layout(column fill-width)
-                component(:data="comp" 
-                          :is="comp.instanceName")
+                component(:data="comp"
+                          :index="index"
+                          :is="comp.instanceName"
+                          :key="comp.id")
                 edit-sep(:index="index"
                         v-on:edit-sep-new="editSepNew"
                         :show="canShowInsert(index)")
     template(v-else v-for="(comp, index) in elements")
       component(:data="comp" 
+                :index="index"
                 :is="comp.instanceName" 
                 :key="comp.id")
 </template>
@@ -45,6 +49,7 @@ export default {
     "document-item": () => import("./Document.vue"),
     "edit-sep": () => import("../Compound/EditSeparator.vue"),
     "edit-item-menu": () => import("../Compound/EditItemMenu.vue"),
+    "text-item": () => import("./Text.vue"),
     draggable
   },
   extends: BaseElement,
@@ -56,6 +61,7 @@ export default {
   },
   methods: {
     editSepNew: function(index, type) {
+      console.log(type);
       this.insertElement(index, type);
     },
     editSepDel: function(index) {
@@ -66,10 +72,20 @@ export default {
       this.showInsert = index;
     },
     canShowMenu: function(index) {
-      return this.showMenu == index;
+      return this.showMenu == index && index > 0;
     },
     canShowInsert: function(index) {
       return Math.abs(this.showInsert - index) <= 1;
+    }
+  },
+  computed: {
+    elements: {
+      get: function() {
+        return this.$store.getters.courseTab.elements;
+      },
+      set: function(value) {
+        this.$store.commit("updateCourseTabElements", value);
+      }
     }
   }
 };
