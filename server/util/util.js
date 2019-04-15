734f9b2bac66c4178f24ff5cf201ead94a01b475
zachbryant <zbryant832@gmail.com>
2019-04-14 20:27:04 -0400

    uuidv4 method

--- server/util/util.js
+++ server/util/util.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
+const uuidv4 = require("uuid/v4");
+
 
 module.exports.isValidEmail = function(email) {
   const emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
   return emailPattern.test(String(email).toLowerCase());
+};
+
+module.exports.getUUIDV4 = function() {
+  return uuidv4();
 };
