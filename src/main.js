8a9722f92056409f8a2f294aca8d7e1930ee0a47
zachbryant <zbryant832@gmail.com>
2019-04-17 22:35:30 -0400

    minifixed css fix

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
 import "chart.js";
 import "hchs-vue-charts";
-import 'vue2-dropzone/dist/vue2Dropzone.css';
+import "vue2-dropzone/dist/vue2Dropzone.min.css";
 
 import App from "./App.vue";
 import store from "./store";
