9c05ecec73a50dc1b3296110c3d92b81d4d64b63
Zach Bryant <zbryant832@gmail.com>
2019-04-19 19:02:59 -0400

    imports

--- src/views/Coursepage.vue
+++ src/views/Coursepage.vue
@@ -1,25 +1,27 @@
 <template>
   <div>
-    <Title v-bind:msg="this.courseIndex"/>
-    <Calendar v-bind:msg="this.courseIndex"/>
-    <Announcements v-bind:msg="this.courseIndex"/>
-    <Contact v-bind:msg="this.courseIndex"/>
-    <Info v-bind:msg="this.courseIndex"/>
-    <Resources v-bind:msg="this.courseIndex"/>
-    <Grade v-bind:msg="this.courseIndex"/>
+    <Title v-bind:msg="this.courseIndex" />
+    <Calendar v-bind:msg="this.courseIndex" />
+    <Announcements v-bind:msg="this.courseIndex" />
+    <Contact v-bind:msg="this.courseIndex" />
+    <Info v-bind:msg="this.courseIndex" />
+    <Resources v-bind:msg="this.courseIndex" />
+    <Grade v-bind:msg="this.courseIndex" />
   </div>
 </template>
 
 <script>
-import Courses from "@/components/Courses.vue";
-import Title from "@/components/Title.vue";
-import CourseService from "../services/CourseService";
-import Calendar from "@/components/Calendar.vue";
-import Grade from "@/components/Grade.vue";
-import Announcements from "@/components/Announcements.vue";
-import Contact from "@/components/Contact.vue";
-import Info from "@/components/Info.vue";
-import Resources from "@/components/Resources.vue";
+import {
+  Courses,
+  Title,
+  CourseService,
+  Calendar,
+  Grade,
+  Announcements,
+  Contact,
+  Info,
+  Resources
+} from "@/components/componentImports.js";
 
 export default {
   name: "coursepage",
@@ -58,18 +60,18 @@ export default {
       var courseName = urlName.replace("%20", " ");
       console.log("url coursename = " + courseName);
 
-      this.obj= await CourseService.getOneCourse(courseName);
+      this.obj = await CourseService.getOneCourse(courseName);
 
-      for(var i=0;i<this.courses.length;i++){
-        
-        if((this.courses[i]._id.localeCompare(this.obj))==0){
+      for (var i = 0; i < this.courses.length; i++) {
+        if (this.courses[i]._id.localeCompare(this.obj) == 0) {
           //this.store.commit("setCourseIndex", {i});
           this.courseIndex = i;
         }
-        
-       console.log("course id = " + this.courses[i]._id + " course name = " + this.obj)
+
+        console.log(
+          "course id = " + this.courses[i]._id + " course name = " + this.obj
+        );
       }
-      
     } catch (err) {
       this.error = err.message;
     }
@@ -95,5 +97,3 @@ export default {
   }
 };
 </script>
-
-
