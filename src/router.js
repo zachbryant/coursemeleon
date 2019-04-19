fd3876fbcb01b6e3e45fdaa21c570d51456ad33a
Zach Bryant <zbryant832@gmail.com>
2019-04-18 21:45:02 -0400

    reload home when click course creation

--- src/router.js
+++ src/router.js
@@ -48,7 +48,7 @@ let router = new Router({
     {
       path: "/create",
       name: "create",
-      component: Home,
+      component: () => import("./views/Home.vue"),
       meta: {
         requiresAuth: true
       },
@@ -64,6 +64,10 @@ let router = new Router({
 });
 
 router.beforeEach((to, from, next) => {
+  store.commit("setErrorMessage", "");
+  store.commit("setWarningMessage", "");
+  store.commit("setInfoMessage", "");
+  store.commit("setSuccessMessage", "");
   if (to.matched.some(record => !!record.meta.requiresAuth)) {
     console.log(to);
     if (store.getters.isLoggedIn) {
