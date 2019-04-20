d7e1753e8528d68886d960121abc95fbee97d0f1
Zach Bryant <zbryant832@gmail.com>
2019-04-19 22:10:30 -0400

    removed unused imports

--- src/components/componentImports.js
+++ src/components/componentImports.js
@@ -16,15 +16,8 @@ export {
 export { default as Calendar } from "@/components/Course/Compound/Calendar";
 export { default as Contact } from "@/components/Course/Compound/Contact";
 export { default as Course } from "@/components/Course/Course";
-export { default as Courses } from "@/components/Course/Compound/Courses";
 export { default as CourseService } from "@/services/CourseService";
-export {
-  default as CourseInformation
-} from "@/components/Course/Compound/CourseInformation";
 export { default as Grade } from "@/components/Course/Compound/Grade";
-export { default as Resources } from "@/components/Course/Compound/Resources";
-export { default as Info } from "@/components/Course/Compound/Info";
-export { default as Title } from "@/components/Course/Compound/Title";
 export {
   default as EditSeparator
 } from "@/components/Course/Compound/EditSeparator";
